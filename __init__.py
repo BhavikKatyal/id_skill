723d160d0a7b030cea517ef3d5611ad5c23b77ce
BhavikKatyal <40070017+BhavikKatyal@users.noreply.github.com>
2019-02-12 12:19:07 +0530

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -18,10 +18,16 @@ class D_Skill(MycroftSkill):
         try:
             number = message.data.get("number")
             response = {'number': message.data.get("number")}
+            b=""
+            
+            for x in range(0,len(a)):
+                if(a[x]!=" "):
+                    b=b+a[x]
+                        
             url = "https://xrrj3ql5ud.execute-api.us-east-1.amazonaws.com/Test/mycroft-skill-empno"
 
             key="{\n\t\"inputparams\":"
-            value="MTX"+number
+            value="MTX"+b
             key1 = "\""+value+"\"\n}"
             payload = key + key1
             headers = {
